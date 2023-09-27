81d5d696e769d28c145e4bec3a409cbc28b27b38
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-09-27 20:36:04 -0300

    PasswordEncoder

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$o/FpGQYQfz/DnLnGH4dwd.xCHGd1KUR3vxaQxqo3ukME/E2k0.VrC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$o/FpGQYQfz/DnLnGH4dwd.xCHGd1KUR3vxaQxqo3ukME/E2k0.VrC');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
